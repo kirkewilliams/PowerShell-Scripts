d1493e29f8a4721813a5000526a65d0fa1fec8f5
Kirk Williams <74762207+kirkewilliams@users.noreply.github.com>
2025-07-29 13:37:05 +0200

    .NET Scripts

---
+++ .NET/Get-DotNetVersion.ps1
@@ -0,0 +1,1 @@
+Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where-Object { $_.PSChildName -Match '^(?!S)\p{L}'} | Select-Object PSChildName, version
